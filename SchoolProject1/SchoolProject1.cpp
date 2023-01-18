57a7ac52c63c0c765b5afdfc90c7110cd59c3da5
SonicTheHedgehog <97832164+Sonic-TheHedgehog@users.noreply.github.com>
2023-01-18 20:23:31 +0300

    mouse in box

--- SchoolProject1/SchoolProject1.cpp
+++ SchoolProject1/SchoolProject1.cpp
@@ -3,27 +3,14 @@
 #include "Rand64.h"
 
 
-BOOL CALLBACK Windows(HWND hwnd, LPARAM lparam)
-{
-	if (IsWindowVisible(hwnd)) {
-		RECT r;
-		GetWindowRect(hwnd, &r);
-		Random64 random(GetTickCount64());
-		r.left += random(-2, 2);
-		r.right += random(-2, 2);
-		r.top += random(-2, 2);
-		r.bottom += random(-2, 2);
-		MoveWindow(hwnd, r.left, r.top, r.right - r.left, r.bottom - r.top , true);
-	}
-	return true;
-}
-
 int main()
 {
-	while (true)
-	{
-		EnumWindows((WNDENUMPROC)Windows, 0);
-		Sleep(10);
-	}
+	RECT r;
+	r.left = 10;
+	r.top = 10;
+	r.bottom = GetSystemMetrics(SM_CYSCREEN) / 2;
+	r.right = GetSystemMetrics(SM_CXSCREEN) / 2;
+	ClipCursor(&r);
+
 
 }
